175d241e2b7c927987f21e9e39282198575afb22
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-12-24 16:01:29 +0100

    hash password using bcryptjs

--- Back/src/Models/Users.js
+++ Back/src/Models/Users.js
@@ -1,13 +1,23 @@
 const mongoose = require("mongoose");
+const bcrypt=require('bcryptjs')
 
 const UserSchema = new mongoose.Schema({
   username: String,
   email: String,
   password:{
     type:String,
-    required:true
+    required:true,
+    // validate(v){
+    //   if(!v.lenght<3 )
+    //   {
+
+    //   }
+    // }
   }
 });
 
+UserSchema.pre('save',async function(){
+  if(this.isModified('password')) this.password = await bcrypt.hash(this.password,8)
+})
 const UserModel = mongoose.model("User", UserSchema);
 module.exports = UserModel;
