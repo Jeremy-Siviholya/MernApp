d6401b1ceee8b32c6cd7908be74213f25e02e6ae
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-12-26 12:18:18 +0100

    work on part pagination

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -5,33 +5,31 @@ import "react-toastify/dist/ReactToastify.css";
 import Upbar from './components/Upbar/Upbar';
 import { Pagination } from './components/Users/Pagination';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom'
-import UpdateUser from './components/Users/updateUser';
 import AnimateRoutes from './components/Routes/animateRoutes';
+
 
 
 const App = () => {
   return (
     <Router>
-      <AnimateRoutes/>
-     
-     <div className="bg-[#323232] flex justify-center relative items-center h-screen w-screen">
-      <Upbar/>
-
-      <Users />
-      <Pagination/>
-      <ToastContainer
-        position="bottom-left"
-        autoClose={5000}
-        hideProgressBar={false}
-        newestOnTop={false}
-        closeOnClick
-        rtl={false}
-        pauseOnFocusLoss
-        draggable
-        pauseOnHover
-        theme='dark'
-      />
-    </div>
+      <AnimateRoutes />
+      <div className="bg-[#323232] flex justify-center relative items-center h-screen w-screen">
+        <Upbar />
+        <Users />
+        <Pagination />
+        <ToastContainer
+          position="bottom-left"
+          autoClose={5000}
+          hideProgressBar={false}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+          theme="dark"
+        />
+      </div>
     </Router>
   );
 }
