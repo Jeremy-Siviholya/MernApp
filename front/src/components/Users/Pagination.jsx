614b2e6f32c6a29f1b16c4c62f976e4df9ae3894
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2023-12-26 13:57:04 +0100

    work on part pagination

--- front/src/components/Users/Pagination.jsx
+++ front/src/components/Users/Pagination.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
 
 export const Pagination = () => {
+  
   return (
     <div className="bg-black/50 backdrop-blur-md border border-gray-600 h-20 fixed bottom-3 w-[840px] rounded-md flex justify-between">
-      
+
     </div>
   );
 }
