25e812c3172f253377dd66e92e95bf50e2bdead7
Jeremy-Siviholya <jeremysiviholya7@outlook.fr>
2024-01-02 12:47:04 +0100

    work on part users

--- Back/app.js
+++ Back/app.js
@@ -9,9 +9,6 @@ app.listen(port, console.log(`listening on port http://localhost:${port}`));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
-
 Connectdb().catch((err) => console.log(err));
-
 const UsersRoute = require("./src/routes/Users");
-
 app.use(UsersRoute);
