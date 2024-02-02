8ffb9d38cff1f635032a341a1cbc57d74ad53aa0
jeremy-siviholya <jeremysiviholya7@outlook.fr>
2024-02-02 18:33:07 +0100

    add build command

--- Back/app.js
+++ Back/app.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const port = process.env.PORT || 7778;
+// const port = process.env.PORT || 7778;
 
-app.listen(port, console.log(`listening on port http://localhost:${port}`));
+// app.listen(port, console.log(`listening on port http://localhost:${port}`));
 app.use(cors());
 app.use(express.json());
 app.use(express.static("public"));
